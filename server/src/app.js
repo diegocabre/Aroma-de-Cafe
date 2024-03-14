bad411d858e256c7f43901d0075ed7836f5e1a55
Jose Corredor <jos3.corredor@gmail.com>
2024-03-13 21:15:58 -0500

    Agrego ordenamiento de carpetas y estructura base del servidor

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const routes = require('./routes/index');
+
+const app = express();
+
+app.use(express());
+app.use(express.json());
+app.use(morgan());
+app.use(cors());
+
+app.use('/',routes);
+
+module.exports = app;
+
