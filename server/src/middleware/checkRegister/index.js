48baf99a720344bb8a162e64f0d43756ad62f0d7
Diego Cabre <diegocabre@gmail.com>
2024-03-28 18:41:19 -0300

    index server

--- server/src/middleware/checkRegister/index.js
+++ server/src/middleware/checkRegister/index.js
@@ -1,95 +1,89 @@
-const db = require('../../database/db');
+const db = require("../../database/db");
 const jwt = require("jsonwebtoken");
-const { check, validationResult} = require('express-validator');
-const format = require('pg-format')
-const {getCorreo}= require('../../database/querys/querys');
-const {KEYTOKEN} =process.env;
+const { check, validationResult } = require("express-validator");
+const format = require("pg-format");
+const { getCorreo } = require("../../database/querys/querys");
+const { KEYTOKEN } = process.env;
 
 const validationFieldRegistrer = [
-    check('name')
-		.trim()
-        .notEmpty()
-		.isLength({ min: 3 })
-		.withMessage("El nombre es requerido con minimo 3 caracteres")
-        .isString()
-		.withMessage("El nombre es requerido y debe ser un string")
-		.custom(value => /^[a-zA-Z]+$/.test(value))
-    	.withMessage('El nombre debe contener solo letras'),
-    check('lastName')
-		.trim()
-        .notEmpty()
-		.isLength({ min: 3 })
-		.withMessage("El nombre es requerido con minimo 3 caracteres")
-        .isString()
-		.withMessage("El apellido es requerido y debe ser un string")
-		.custom(value => /^[a-zA-Z]+$/.test(value))
-    	.withMessage('El apellido debe contener solo letras'),
-	check('email')
-		.trim()
-		.notEmpty()
-		.isEmail(),
-    check('password')
-		.notEmpty()
-		.isLength({ min: 8 })
-		.custom((value,{req})=>{
-			const passwordregex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-			if(passwordregex.test(value)){
-				return true
-			}
-			else{
-				throw new Error ('La contraseña debe tener al menos una mayuscula, una minuscula, un numero y un caracter especial como @$!%*?& y 8 caracteres como minimo')
-			}
-		}),
-(req, res, next) => {
-	try {
-        const {password,passwordConfir} = req.body
-        if(password != passwordConfir){
-            res.status(400).json({
-                msg: "No coincide la contraseña y la confirmación"
-            })
-        }
-        else{
-            validationResult(req).throw()
-            return next()
-        }
-		
-	} catch (error) {
-		return res.status(400).send({error: error.array()})
-		
-	}
-}
-]
-const validationCorreo = async(req,res,next)=>{
-	const {email} = req.body;
-	try {
-		const value =[email];
-		const query = format(getCorreo,...value)
-		const data = await db.query(query)
-		if(data.rows.length){
-			return res.status(409).json({
-				error: "bad request",
-				msg: "El usuario ya está registrado"
-			})
-		}
-		else{
-		return next()
-	}
-	} catch (error) {
-		return res.status(500).json(error)
-	}
-}
+  check("name")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 3 })
+    .withMessage("El nombre es requerido con minimo 3 caracteres")
+    .isString()
+    .withMessage("El nombre es requerido y debe ser un string")
+    .custom((value) => /^[a-zA-Z]+$/.test(value))
+    .withMessage("El nombre debe contener solo letras"),
+  check("lastName")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 3 })
+    .withMessage("El nombre es requerido con minimo 3 caracteres")
+    .isString()
+    .withMessage("El apellido es requerido y debe ser un string")
+    .custom((value) => /^[a-zA-Z]+$/.test(value))
+    .withMessage("El apellido debe contener solo letras"),
+  check("email").trim().notEmpty().isEmail(),
+  check("password")
+    .notEmpty()
+    .isLength({ min: 8 })
+    .custom((value, { req }) => {
+      const passwordregex =
+        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      if (passwordregex.test(value)) {
+        return true;
+      } else {
+        throw new Error(
+          "La contraseña debe tener al menos una mayuscula, una minuscula, un numero y un caracter especial como @$!%*?& y 8 caracteres como minimo"
+        );
+      }
+    }),
+  (req, res, next) => {
+    try {
+      const { password, passwordConfir } = req.body;
+      if (password != passwordConfir) {
+        res.status(400).json({
+          msg: "No coincide la contraseña y la confirmación",
+        });
+      } else {
+        validationResult(req).throw();
+        return next();
+      }
+    } catch (error) {
+      return res.status(400).send({ error: error.array() });
+    }
+  },
+];
+const validationCorreo = async (req, res, next) => {
+  const { email } = req.body;
+  try {
+    const value = [email];
+    const query = format(getCorreo, ...value);
+    const data = await db.query(query);
+    if (data.rows.length) {
+      return res.status(409).json({
+        error: "bad request",
+        msg: "El usuario ya está registrado",
+      });
+    } else {
+      return next();
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
 
-const verifytoken =async(req,res,next)=>{
-	try {
-		const Authorization = req.header("Authorization");
-		const token = Authorization.split("Bearer ")[1];
-		jwt.verify(token, KEYTOKEN);
-		console.log("token verificado")
-		return next();
-	} catch (error) {
-		return res.status(401).send({error: error})
-	}
+const verifytoken = async (req, res, next) => {
+  try {
+    const Authorization = req.header("Authorization");
+    const token = Authorization.split("Bearer ")[1];
+    jwt.verify(token, KEYTOKEN);
+    console.log("token verificado");
+    return next();
+  } catch (error) {
+    return res.status(401).send({ error: error });
+  }
+};
 
-}
-
-module.exports = {validationCorreo,validationFieldRegistrer,verifytoken}
+module.exports = { validationCorreo, validationFieldRegistrer, verifytoken };
