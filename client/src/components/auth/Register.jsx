548d07ef9ef252280c03733b043fea547401f21d
Diego Cabre <diegocabre@gmail.com>
2024-04-02 17:57:24 -0300

    Conexion con el Server

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,32 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../css/Register.css";
 
 function Register() {
+  const [validated, setValidated] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValidated({ ...validated, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(validated),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setValidated({
+        name: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+    }
+  };
+
   return (
     <div className="form-container">
       <h1>Registrate</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Tu Nombre" />
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Tu Nombre"
+            onChange={handleChange}
+            value={validated.name}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicLastName">
           <Form.Label>Apellido</Form.Label>
-          <Form.Control type="text" placeholder="Tu Apellido" />
+          <Form.Control
+            type="text"
+            name="lastName"
+            placeholder="Tu Apellido"
+            onChange={handleChange}
+            value={validated.lastName}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="tu Correo Electrónico" />
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="tu Correo Electrónico"
+            onChange={handleChange}
+            value={validated.email}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Contraseña</Form.Label>
-          <Form.Control type="password" placeholder="tu Contraseña" />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="tu Contraseña"
+            onChange={handleChange}
+            value={validated.password}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Confirmar Contraseña</Form.Label>
-          <Form.Control type="password" placeholder="tu Contraseña de nuevo" />
+          <Form.Control
+            type="password"
+            name="confirmPassword"
+            placeholder="tu Contraseña de nuevo"
+            onChange={handleChange}
+            value={validated.confirmPassword}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Registrate
